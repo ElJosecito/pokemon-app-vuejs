8a6ee4e55eaa1cecc70df10e5209c5c2beacbcb0
ElJosecito <imjose0314@gmail.com>
2023-11-01 12:59:08 -0400

    pokemon-app-vuejs

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,59 @@
+<template lang="">
+    <div>
+        <div class="title-container">
+            <h1>Lista de Pokemon</h1>
+        </div>
+        
+        <div class="pokemon_container">
+            <PokemonComponent v-for="pokemon in Pokemon" :key="pokemon.id" :name="pokemon.name" :img="pokemon.sprites.front_default" />
+        </div>
+
+        <div class="buttons">
+            <button @click="getPokemon">Get Pokemon</button>
+            <button @click="getMorePokemon">Get 20 more</button>
+        </div>
+    </div>
+</template>
+<script>
+import PokemonComponent from './PokemonComponent.vue';
+
+export default {
+    name: 'HomeComponent',
+    data() {
+        return {
+            Pokemon: [],
+            count: 20,
+        }
+    },
+    components: {
+        PokemonComponent,
+    },
+
+    methods: {
+        async getPokemon() {
+
+            let pokemonData = [];
+
+            for (let i = 1; i <= this.count; i++) {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+                const data = await response.json();
+
+                pokemonData.push(data);
+            }
+
+            //console.log(pokemonData);
+            this.Pokemon = pokemonData;
+        },
+
+        async getMorePokemon() {
+            this.count += 20;
+            this.getPokemon();
+        }
+
+    },
+
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/components/PokemonComponent.vue
@@ -0,0 +1,34 @@
+<template lang="">
+    <div class="pokemon_card" @click="showData">
+        <img :src="img" :alt="name" />
+        <h3>{{name}}</h3>
+    </div>
+</template>
+<script>
+export default {
+    name: 'PokemonComponent',
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        img: {
+            type: String,
+            required: true,
+        }
+    },
+
+    methods: {
+        async showData() {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.name}`);
+            const data = await response.json();
+
+            console.log(data);
+        }
+    },
+    
+}
+</script>
+<style lang="">
+    
+</style>
